41b12fdf213610c4ab7e0bf1aeea201205ce89f6
Niour <38693643+Niour@users.noreply.github.com>
2019-11-05 10:07:31 +0200

    Finishing Http Part
    Last finishing touches.
    Added a resolver to the routing Module ( we could add a authenticator instead )

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.module';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -17,7 +17,7 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.http
+        return this.http
         .get<Recipe[]>('https://ng-burger-181e7.firebaseio.com/recipes.json')
         .pipe(
             map(recipes => {
@@ -26,9 +26,10 @@ export class DataStorageService {
                     ingredients: recipe.ingredients ? recipe.ingredients : []
                 };
             });
-        }))
-        .subscribe(recipes => {
+        }),
+        tap(recipes => {
             this.recipeService.setRecipes(recipes);
-        });
+        })
+        );
     }
 }
