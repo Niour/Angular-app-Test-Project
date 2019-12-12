43a60a46ecbcfb569b18a88683f8492df82cdd1c
Niour <38693643+Niour@users.noreply.github.com>
2019-12-12 16:05:47 +0200

    some Error Catching

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 interface AuthResponseData {
     idToken: string;
@@ -15,7 +17,8 @@ export class AuthService {
 
     signup(email: string, password: string) {
         return this.http.
-            post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBdWPSTRRjFUZgR2tB3V0GS0p-cvCqK1SU',
+            post<AuthResponseData>(
+                'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCaMz7T8Edxmht61pbsOxL_0atHIN5dqzw',
             {
                 // tslint:disable-next-line: object-literal-shorthand
                 email: email,
@@ -23,6 +26,18 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true
             }
-        );
+        ).pipe(catchError(errorRes => {
+            console.log(errorRes);
+            let errorMessage = 'An unknown error occured';
+            if (!errorRes.error || !errorRes.error.error) {
+                console.log('YEs');
+                return throwError(errorMessage);
+            }
+            switch (errorRes.error.error.message) {
+                case 'EMAIL_EXISTS':
+                    errorMessage = 'This email exists already';
+            }
+            return throwError(errorMessage);
+        }));
     }
 }
